7f10d8033655a6bbfd0d289571cd6fa0d8ee9f96
Jasmine Li <lijasminej@nbp-200-116.nbp.ruw.rutgers.edu>
2016-03-21 21:22:29 -0400

    Added footer into html

--- assets/js/nyt.js
+++ assets/js/nyt.js
@@ -144,3 +144,9 @@ function displayKeywords() {
     };
   };
 };
+
+$(document).ready(function(){
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+});
