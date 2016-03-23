1494f5835bd02c259bf1e305e2a51f311ad0e974
Jasmine Li <lijasminej@nbp-207-22.nbp.ruw.rutgers.edu>
2016-03-23 18:20:18 -0400

    Minor style changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
+  // Change placeholder text on resize
+
+  $('#search').each(function() {
+    $(this).data('placeholder', $(this).attr('placeholder'));
+  });
+
+  function changePlaceholder() {
+      if( $(window).width() <= 640){
+          $('#search').attr('placeholder','Search');
+      } else {
+          $('#search').each(function() {
+              $(this).attr('placeholder', $(this).data('placeholder'));
+          });
+      }   
+  }
+
+  $(window).resize( changePlaceholder ).trigger('resize');
+
+  // Clear input
+
+  document.getElementById("clearinput").onclick = function() {
+    $('#search').val("");
+  };
+
+  // Run initial api calls
+
 	document.getElementById("searchbar").onsubmit = function() {
 		fixed();
 		search();
