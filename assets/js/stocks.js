5414beb199b3989518e141f3d0f6c2809dc33301
ltdelia <delia.louis@gmail.com>
2016-07-26 23:47:04 -0400

    removed cache from ajax

--- assets/js/stocks.js
+++ assets/js/stocks.js
@@ -54,7 +54,6 @@ function getSymbol() {
     // url: "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=" + stockInfo.input, // why no work???
 
     method: 'GET',
-    cache: true,
     dataType : 'jsonp',
     jsonpCallback: 'getData',
     crossdomain:true
